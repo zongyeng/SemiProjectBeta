369e8fbcc103c5846d38f2568fb5fedeb881a5fe
김현호 <h2kim59@naver.com>
2019-08-07 21:12:37 +0900

    - MusicDao.java AlbumDao.java insert, delete, update session.commit(); 추가 - MusicBiz.java, AlbumBiz.java 수정

--- src/semi/view/MusicTest.java
+++ src/semi/view/MusicTest.java
@@ -2,16 +2,18 @@ package semi.view;
 
 import java.util.List;
 
+import semi.biz.AlbumBiz;
 import semi.biz.MusicBiz;
+import semi.dto.AlbumDto;
 import semi.dto.MusicDto;
 
 public class MusicTest {
 
 	public static void main(String[] args) {
 		
-		MusicBiz biz = new MusicBiz();
+/*			MusicBiz biz = new MusicBiz();
 		MusicDto dto = new MusicDto();
-		
+	
 		List<MusicDto> list = biz.selectList_music();
 		System.out.println(list);
 		//for(MusicDto dto : list) {
@@ -53,10 +55,21 @@ public class MusicTest {
 
 		List<MusicDto> list2 = biz.selectList_music();
 		System.out.println(list2);
-	
+
 		dto.setMusic_seq(1);
+		dto.setMusic_artist(1);
+		dto.setMusic_no(1);
+		dto.setMusic_title("3_FUNCKEY MUSIC");
 		dto.setMusic_hl_time(30);
-		dto.setMusic_composer("이순신");
+		dto.setMusic_size(10);
+		dto.setMusic_length(310);
+		dto.setMusic_composer("조영찬");
+		dto.setMusic_lyricist("조영찬");
+		dto.setMusic_content("FUNCKEY MUSIC");
+		dto.setMusic_tag("FUNCKEY MUSIC");
+		dto.setMusic_enable('Y');
+		dto.setMusic_album(1);
+		
 		int res2 = biz.update_music(dto);
 		if(res2 > 0) {
 			System.out.println("update_music 성공");
@@ -66,7 +79,68 @@ public class MusicTest {
 
 		dto = biz.selectOne_music(1);
 		System.out.println(dto);
+*/			
+
+		AlbumBiz biza = new AlbumBiz();
+		AlbumDto dto = new AlbumDto(); 
+/*		 	
+		List<AlbumDto> list = biza.selectList_album();
+		System.out.println(list);
+		//for(MusicDto dto : list) {
+		//	System.out.println(dto);
+		//}
+				
+		dto = biza.selectOne_album(1);
+		System.out.println(dto);
 		
+		dto.setAlbum_artist(3);
+		dto.setAlbum_no(1);
+		dto.setAlbum_title("다비치 - 이 사랑");
+		dto.setAlbum_tracks(10);
+		dto.setAlbum_length(310);
+		dto.setAlbum_content("좋은노래");
+		dto.setAlbum_tag("가요");
+		dto.setAlbum_enable('Y');
+		
+		int res = biza.insert_album(dto);
+		if(res > 0) {
+			System.out.println("insert_music 성공");
+		} else {
+			System.out.println("insert_music 실패");
+		}
+		
+		List<AlbumDto> list1 = biza.selectList_album();
+		System.out.println(list1);
+		
+		dto.setAlbum_seq(3);
+		dto.setAlbum_artist(1);
+		dto.setAlbum_title("3_FUNCKEY MUSIC");
+		dto.setAlbum_tracks(30);
+		dto.setAlbum_length(250);
+		dto.setAlbum_content("FUNCKEY MUSIC");
+		dto.setAlbum_tag("가요");
+		dto.setAlbum_enable('Y');
+		
+		int res21 = biza.update_album(dto);
+		if(res21 > 0) {
+			System.out.println("update_music 성공");
+		} else {
+			System.out.println("update_music 실패");
+		}
+*/
+		dto = biza.selectOne_album(3);
+		System.out.println(dto);
+
+		int res1 = biza.delete_album(3);
+		if(res1 > 0) {
+			System.out.println("delete_music 성공");
+		} else {
+			System.out.println("delete_music 실패");
+		}
+
+		List<AlbumDto> list2 = biza.selectList_album();
+		System.out.println(list2);
+
 	}
 
 }
