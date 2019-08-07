55ab9a33f92e5f4afb1bd768bca1290800aea712
김현호 <h2kim59@naver.com>
2019-08-07 19:53:04 +0900

    music-mapper.xml, MusicDao.java, Music.Dto.java 수정

--- src/semi/view/MusicTest.java
+++ src/semi/view/MusicTest.java
@@ -17,23 +17,22 @@ public class MusicTest {
 		//for(MusicDto dto : list) {
 		//	System.out.println(dto);
 		//}
-		
-		
-		dto = biz.selectOne_music(0);
+				
+		dto = biz.selectOne_music(1);
 		System.out.println(dto);
 		
 		dto.setMusic_artist(3);
+		dto.setMusic_no(1);
 		dto.setMusic_title("다비치 - 이 사랑");
-		dto.setMusic_composer("홍길동");
+		dto.setMusic_size(10);
+		dto.setMusic_length(310);
+		dto.setMusic_hl_time(0);
 		dto.setMusic_content("좋은노래");
-		dto.setMusic_hl_time(10);
-		dto.setMusic_length(0);
-		dto.setMusic_lylicist("홍길동");
-		dto.setMusic_no(0);
-		dto.setMusic_size(0);
+		dto.setMusic_composer("홍길동");
+		dto.setMusic_lyricist("다비치");
 		dto.setMusic_tag("가요");
 		dto.setMusic_enable('Y');
-		
+		dto.setMusic_album(1);
 		//dto = new MusicDto(null, 3, 1, "좋은노래", 0, 0, 10, "좋은노래", "홍길동", "홍길동", null, "가요","Y",0);
 		int res = biz.insert_music(dto);
 		if(res > 0) {
@@ -42,16 +41,22 @@ public class MusicTest {
 			System.out.println("insert_music 실패");
 		}
 		
-		int res1 = biz.delete_music(1);
+		List<MusicDto> list1 = biz.selectList_music();
+		System.out.println(list1);
+		
+		int res1 = biz.delete_music(12);
 		if(res1 > 0) {
 			System.out.println("delete_music 성공");
 		} else {
 			System.out.println("delete_music 실패");
 		}
-		
-		dto.setMusic_seq(2);
+
+		List<MusicDto> list2 = biz.selectList_music();
+		System.out.println(list2);
+	
+		dto.setMusic_seq(1);
 		dto.setMusic_hl_time(30);
-		dto.setMusic_lylicist("이순신");
+		dto.setMusic_composer("이순신");
 		int res2 = biz.update_music(dto);
 		if(res2 > 0) {
 			System.out.println("update_music 성공");
@@ -59,6 +64,9 @@ public class MusicTest {
 			System.out.println("update_music 실패");
 		}
 
+		dto = biz.selectOne_music(1);
+		System.out.println(dto);
+		
 	}
 
 }
