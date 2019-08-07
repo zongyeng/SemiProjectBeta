5e6d24237f184a212b14c9ea65828f90d0511f68
KOKOPA <kssp1236@gmail.com>
2019-08-07 21:48:21 +0900

    ggggg

--- src/semi/biz/loginBiz.java
+++ src/semi/biz/loginBiz.java
@@ -9,14 +9,16 @@ public class loginBiz {
 
 	loginDao dao = new loginDao();
 	
-	public boolean signup(int userinfo_controlno,String userinfo_name,
+	public boolean signup(String userinfo_name,
 			String userinfo_nickname, String userinfo_id, String userinfo_password,
-			char userinfo_sex,int userinfo_age,int userinfo_phonenumber,String userinfo_email) {
-		return dao.signup(userinfo_controlno, userinfo_name, userinfo_nickname, userinfo_id, userinfo_password, userinfo_sex, userinfo_age, userinfo_phonenumber, userinfo_email);
+			char userinfo_sex,int userinfo_age,String userinfo_phonenumber,String userinfo_email) {
+		
+		return dao.signup(userinfo_name, userinfo_nickname, userinfo_id, userinfo_password, userinfo_sex, userinfo_age, userinfo_phonenumber, userinfo_email);
 	}
 	public loginDto login(String id , String password) {
 		return dao.login(id, password);
 	}
+	
 }
 
 	
